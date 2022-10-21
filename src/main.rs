b327e0008a05779c332e3356b911690e2e5d939f
Dex <sheepareawesome777@gmail.com>
2022-10-21 11:25:18 -0400

    Boilerplate stuff

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+/*
+**	I really don't know what I'm doing.
+**	- Dex 10/21/2022
+*/
+
+// Hello World
+// ++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.
+
+use std::env;
+
+fn main() {
+	// Get arguments
+	let args: Vec<String> = env::args().collect();
+	
+	// 
+}
