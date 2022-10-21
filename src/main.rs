5789416f5a33f65980739a6cf9258196b9bb6935
Dex <sheepareawesome777@gmail.com>
2022-10-21 12:01:08 -0400

    Added memory cells vector

--- src/main.rs
+++ src/main.rs
@@ -13,4 +13,23 @@ fn main() {
 	// Get arguments
 	let args: Vec<String> = env::args().collect();
 	let program = if args.len() > 1 { &(args[1])[..] } else { HELLO_WORLD_PROGRAM };
+
+	// Index of pointer
+	let mut ptr_i: u8 = 0;
+
+	let mut memory: Vec<u8> = vec![0;256];
+
+	for instruction in program.chars() {
+		match instruction {
+			'>' => ptr_i += 1,
+			'<' => ptr_i -= 1,
+			'+' => (),
+			'-' => (),
+			'.' => (),
+			',' => (),
+			'[' => (),
+			']' => (),
+			_ => ()
+		}
+	}
 }
