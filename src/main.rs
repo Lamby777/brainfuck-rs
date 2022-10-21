369a7242521cfa89cf4ecad9b4134b81938a6445
Dex <sheepareawesome777@gmail.com>
2022-10-21 11:50:13 -0400

    Get program code

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@
 **	- Dex 10/21/2022
 */
 
-// Hello World
-// ++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.
-
 use std::env;
+
+// Used if args left empty
+const HELLO_WORLD_PROGRAM: &str =
+	"++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
 
 fn main() {
 	// Get arguments
 	let args: Vec<String> = env::args().collect();
-	
-	// 
+	let program = if args.len() > 1 { &(args[1])[..] } else { HELLO_WORLD_PROGRAM };
 }
