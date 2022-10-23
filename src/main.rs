b85fe639a3e7f20f7e114e138c1b56ec052db2d1
Dex <sheepareawesome777@gmail.com>
2022-10-23 18:10:29 -0400

    Implementing Loops (Part 1)

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 
 use std::env;
 use std::num::Wrapping;
+use bimap::*;
 
 // Used if args left empty
 const HELLO_WORLD_PROGRAM: &str =
@@ -13,33 +14,60 @@ const HELLO_WORLD_PROGRAM: &str =
 fn main() {
 	// Get arguments
 	let args: Vec<String>	= env::args().collect();
-	let programstr			= if args.len() > 1 { &(args[1])[..] } else { HELLO_WORLD_PROGRAM };
+	let programstr: &str	= if args.len() > 1 { &(args[1])[..] } else { HELLO_WORLD_PROGRAM };
 	let program: Vec<char>	= programstr.chars().collect();
+
+	let mut loop_cache: BiHashMap<usize, usize> = BiMap::new();
+
 
 	// Index of pointer
 	let mut ptr_i:	Wrapping<u8>	= Wrapping(0);
 	let mut step:	usize			= 0;
 
-	let mut memory: Vec<Wrapping<u8>> = vec![Wrapping(0);256];
+	let mut memory: Vec<Wrapping<u8>> = vec![Wrapping(0); 256];
 
 	
 	while step < program.len() {
-		let mut next_step = step + 1;
+		let mut next_step: usize = step + 1;
 
 		match program[step] {
+			// Move pointer
 			'>' => ptr_i += 1,
 			'<' => ptr_i -= 1,
+
+			// Modify cell
 			'+' => memory[(ptr_i).0 as usize] += 1,
 			'-' => memory[(ptr_i).0 as usize] -= 1,
+
+			// Print Cell -> ASCII Char
 			'.' => {
-				let cell_value = memory[ptr_i.0 as usize].0;
-				//let ch = char::from_u32(cell_value as u32, 10).unwrap();
-				let ch = cell_value as char;
-				print!("{}", ch)
+				let asciiv: u8 = memory[ptr_i.0 as usize].0;
+				print!("{}", asciiv as char);
 			},
+
+			// Read ASCII Char -> Cell
 			',' => (),
-			'[' => {next_step = next_step},
-			']' => {next_step = next_step;},
+
+
+
+
+
+			// thank you so much http://brainfuck.org/brainfuck.html
+			// for helping my brain not explode while coding this part
+			'[' => {
+				// If ptr == 0 skip past ]
+				if (memory[(ptr_i).0 as usize]).0 == 0 {
+					next_step = next_step;
+				}
+			},
+
+			']' => {
+				// If ptr != 0 skip back to [
+				next_step = next_step;
+			},
+
+			
+			
 			_ => ()
 		}
 
