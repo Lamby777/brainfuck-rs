1ecea7ffd37e0bf04d4b2aea66a4d4e72441f398
Dex <sheepareawesome777@gmail.com>
2022-10-23 19:31:29 -0400

    Loops Part 2 + Debug View Output

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,24 @@
 
 use std::env;
 use std::num::Wrapping;
-use bimap::*;
 
 // Used if args left empty
 const HELLO_WORLD_PROGRAM: &str =
-"++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+"++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
 
 fn main() {
 	// Get arguments
-	let args: Vec<String>	= env::args().collect();
-	let programstr: &str	= if args.len() > 1 { &(args[1])[..] } else { HELLO_WORLD_PROGRAM };
-	let program: Vec<char>	= programstr.chars().collect();
+	let args: Vec<String>			= env::args().collect();
+	let programstr: &str			= if args.len() > 1 {	&(args[1])[..]
+									} else {				HELLO_WORLD_PROGRAM };
+	
+	let program: Vec<char>			= programstr.chars().collect();
 
-	let mut loop_cache: BiHashMap<usize, usize> = BiMap::new();
+	let mut loop_stack: Vec<usize>	= vec![];
 
 
 	// Index of pointer
-	let mut ptr_i:	Wrapping<u8>	= Wrapping(0);
+	let mut ptr_i:	Wrapping<usize>	= Wrapping(0usize);
 	let mut step:	usize			= 0;
 
 	let mut memory: Vec<Wrapping<u8>> = vec![Wrapping(0); 256];
@@ -30,19 +31,21 @@ fn main() {
 	while step < program.len() {
 		let mut next_step: usize = step + 1;
 
+		print!("\n{}\t| {}", step, program[step]);
+
 		match program[step] {
 			// Move pointer
 			'>' => ptr_i += 1,
 			'<' => ptr_i -= 1,
 
 			// Modify cell
-			'+' => memory[(ptr_i).0 as usize] += 1,
-			'-' => memory[(ptr_i).0 as usize] -= 1,
+			'+' => memory[ptr_i.0] += 1,
+			'-' => memory[ptr_i.0] -= 1,
 
 			// Print Cell -> ASCII Char
 			'.' => {
-				let asciiv: u8 = memory[ptr_i.0 as usize].0;
-				print!("{}", asciiv as char);
+				let asciiv: u8 = memory[ptr_i.0].0;
+				print!("{} ", asciiv);
 			},
 
 			// Read ASCII Char -> Cell
@@ -56,14 +59,23 @@ fn main() {
 			// for helping my brain not explode while coding this part
 			'[' => {
 				// If ptr == 0 skip past ]
-				if (memory[(ptr_i).0 as usize]).0 == 0 {
-					next_step = next_step;
+				if (memory[ptr_i.0]).0 == 0 {
+					// Rest of program, after the current step
+					let rest: &str = &programstr[step+1..];
+					next_step = step + get_next_closing_bracket(rest);
+				} else {
+					loop_stack.push(step);
 				}
 			},
 
 			']' => {
 				// If ptr != 0 skip back to [
-				next_step = next_step;
+				if ptr_i.0 != 0 {
+					next_step = *loop_stack.last().unwrap() + 1;
+				} else {
+					loop_stack.pop();
+				}
+				//dbg!(ptr_i.0, step, next_step);
 			},
 
 			
@@ -73,4 +85,24 @@ fn main() {
 
 		step = next_step
 	}
+}
+
+fn get_next_closing_bracket(slice: &str) -> usize {
+	let chars: Vec<char> = slice.chars().collect();
+
+	let mut ct: usize = 1;
+
+	for i in 0..chars.len() {
+		match chars[i] {
+			'['	=> (ct += 1),
+			']'	=> (ct -= 1),
+			_	=> ()
+		};
+
+		if ct == 0 {
+			return i;
+		}
+	};
+
+	panic!("Loop Syntax Error");
 }
